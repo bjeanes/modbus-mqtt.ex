af31b1356c8d2d504c19cb4ccdb47d3dd2027366
Bo Jeanes <me@bjeanes.com>
2026-03-31 14:01:29 +1100

    refactor: extract out Devices context module

---
+++ lib/modbus_mqtt/devices.ex
@@ -0,0 +1,103 @@
+defmodule ModbusMqtt.Devices do
+  @moduledoc """
+  Context module for device and register management.
+  Centralizes queries, changesets, and validation for devices and registers.
+  """
+  import Ecto.Query
+  require Logger
+
+  alias ModbusMqtt.Repo
+  alias ModbusMqtt.Devices.Device
+  alias ModbusMqtt.Devices.Register
+
+  @doc "Lists all active devices with their registers preloaded"
+  def list_active_devices_with_registers do
+    query =
+      from d in Device,
+        where: d.active == true,
+        preload: [:registers]
+
+    Repo.all(query)
+  end
+
+  @doc "Gets a single device by ID with registers preloaded"
+  def get_device!(id) do
+    Repo.get!(Device, id)
+    |> Repo.preload(:registers)
+  end
+
+  @doc "Gets a device by ID without raising if not found"
+  def get_device(id) do
+    case Repo.get(Device, id) do
+      nil -> nil
+      device -> Repo.preload(device, :registers)
+    end
+  end
+
+  @doc "Creates a new device"
+  def create_device(attrs \\ %{}) do
+    %Device{}
+    |> Device.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc "Updates a device"
+  def update_device(%Device{} = device, attrs) do
+    device
+    |> Device.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc "Deletes a device"
+  def delete_device(%Device{} = device) do
+    Repo.delete(device)
+  end
+
+  @doc "Returns a device changeset for use in forms"
+  def change_device(%Device{} = device, attrs \\ %{}) do
+    Device.changeset(device, attrs)
+  end
+
+  @doc "Creates a new register for a device"
+  def create_register(device_id, attrs \\ %{}) do
+    %Register{}
+    |> Register.changeset(Map.put(attrs, "device_id", device_id))
+    |> Repo.insert()
+  end
+
+  @doc "Updates a register"
+  def update_register(%Register{} = register, attrs) do
+    register
+    |> Register.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc "Deletes a register"
+  def delete_register(%Register{} = register) do
+    Repo.delete(register)
+  end
+
+  @doc "Returns a register changeset for use in forms"
+  def change_register(%Register{} = register, attrs \\ %{}) do
+    Register.changeset(register, attrs)
+  end
+
+  @doc "Lists registers for a device"
+  def list_registers_for_device(device_id) do
+    query =
+      from r in Register,
+        where: r.device_id == ^device_id
+
+    Repo.all(query)
+  end
+
+  @doc "Gets a single register by ID"
+  def get_register!(id) do
+    Repo.get!(Register, id)
+  end
+
+  @doc "Gets a register by ID without raising if not found"
+  def get_register(id) do
+    Repo.get(Register, id)
+  end
+end
--- lib/modbus_mqtt/engine/bootstrapper.ex
+++ lib/modbus_mqtt/engine/bootstrapper.ex
@@ -4,11 +4,7 @@ defmodule ModbusMqtt.Engine.Bootstrapper do
   plugs them into the Engine DynamicSupervisor on boot.
   """
   use Task
-  import Ecto.Query
   require Logger
-
-  alias ModbusMqtt.Repo
-  alias ModbusMqtt.Devices.Device
 
   def start_link(_arg) do
     Task.start_link(__MODULE__, :run, [])
@@ -18,12 +14,7 @@ defmodule ModbusMqtt.Engine.Bootstrapper do
     # Delay slightly to ensure Repo is fully ready, or rely on Supervison order
     # (Since this starts after Repo, it should be fine)
 
-    query =
-      from d in Device,
-        where: d.active == true,
-        preload: [:registers]
-
-    devices = Repo.all(query)
+    devices = ModbusMqtt.Devices.list_active_devices_with_registers()
 
     Logger.info("Bootstrapper found #{length(devices)} active device(s), starting engines...")
 
